3f663c61fa708e7900003a88200057578426d169
tanaken0515 <tanaka.kentaro.kk1222@gmail.com>
2020-01-19 14:49:26 +0900

    body is required

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,10 @@
 class Article < ApplicationRecord
   has_rich_text :body
+
+  validate :body_required
+
+  private
+  def body_required
+    errors.add(:body, "is required") unless body.body.present?
+  end
 end
